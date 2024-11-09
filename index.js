5d18eea2f8697f72e92687c565e1de59413f42fa
arnavharekal <githubpai@hotmail.com>
2024-11-09 14:59:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get("/*", (req, res, next) => {
   // Construct the target URL with the request path
-  const targetUrl = "https://rocketbotroyale-106.onrender.com/" + req.path;
+  const targetUrl = "discord.com" + req.path;
 
   https.get(targetUrl, (resp) => {
     res.statusCode = resp.statusCode; // Set the status code to match the response
